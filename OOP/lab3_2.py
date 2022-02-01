78119204d9fe4a3773a440754009e01796a34a5e
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-02-01 13:29:54 +0200

    #

---
+++ OOP/lab3_2.py
@@ -0,0 +1,46 @@
+from lab2_2 import AnyMatrix
+
+class Overload(AnyMatrix):
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __eq__(self, other):
+        val = True
+        if self.rows == other.rows and self.cols == other.cols:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    if self.matrix[i][j] != other.matrix[i][j]:
+                        val = False
+            if val == True:
+                print("Equal")
+            else:
+                print("Not equal")
+        else:
+            print("Can not compare")
+        
+
+    def __add__(self, other):
+        if self.rows == other.rows and self.cols == other.cols:
+            add = [[self.matrix[i][j] + other.matrix[i][j] for i in range(self.cols)] for j in range(self.rows)]
+            return add
+        else:
+            print("Can not add")
+
+    def __sub__(self, other):
+        if self.rows == other.rows and self.cols == other.cols:
+            sub = [[self.matrix[i][j] - other.matrix[i][j] for i in range(self.cols)] for j in range(self.rows)]
+            return sub
+        else:
+            print("Can not substract")
+
+    def __mul__(self, other):
+        if self.rows == other.cols and self.cols == other.rows:
+            result = [[0 for i in range(other.cols)] for j in range(self.rows)]
+            for i in range(self.rows):
+                for j in range(other.cols):
+                    for k in range(other.rows):
+                        result[i][j] += self.matrix[i][k]*other.matrix[k][j]
+            return result
+        else:
+            print("Can not multiply")
+
