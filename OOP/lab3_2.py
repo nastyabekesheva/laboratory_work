6bf1b42a45a577a17becd5205405d9d7c4505fb8
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-02-01 20:11:36 +0200

    #

--- OOP/lab3_2.py
+++ OOP/lab3_2.py
@@ -3,13 +3,15 @@ from lab2_2 import AnyMatrix
 class Overload(AnyMatrix):
     def __init__(self, matrix):
         self.matrix = matrix
+        self.rows = matrix.rows
+        self.cols = matrix.cols
 
     def __eq__(self, other):
         val = True
         if self.rows == other.rows and self.cols == other.cols:
             for i in range(self.rows):
                 for j in range(self.cols):
-                    if self.matrix[i][j] != other.matrix[i][j]:
+                    if self.matrix.get_item(i, j) != other.matrix.get_item(i, j):
                         val = False
             if val == True:
                 print("Equal")
@@ -21,26 +23,26 @@ class Overload(AnyMatrix):
 
     def __add__(self, other):
         if self.rows == other.rows and self.cols == other.cols:
-            add = [[self.matrix[i][j] + other.matrix[i][j] for i in range(self.cols)] for j in range(self.rows)]
+            add = [[self.matrix.get_item(i, j) + other.matrix.get_item(i, j) for i in range(self.cols)] for j in range(self.rows)]
             return add
         else:
             print("Can not add")
 
     def __sub__(self, other):
         if self.rows == other.rows and self.cols == other.cols:
-            sub = [[self.matrix[i][j] - other.matrix[i][j] for i in range(self.cols)] for j in range(self.rows)]
+            sub = [[self.matrix.get_item(i, j) - other.matrix.get_item(i, j) for i in range(self.cols)] for j in range(self.rows)]
             return sub
         else:
             print("Can not substract")
 
     def __mul__(self, other):
         if self.rows == other.cols and self.cols == other.rows:
-            result = [[0 for i in range(other.cols)] for j in range(self.rows)]
+            mul = [[0 for i in range(other.cols)] for j in range(self.rows)]
             for i in range(self.rows):
                 for j in range(other.cols):
                     for k in range(other.rows):
-                        result[i][j] += self.matrix[i][k]*other.matrix[k][j]
-            return result
+                        mul[i][j] += self.matrix.get_item(i, k)*other.matrix.get_item(k, j)
+            return mul
         else:
             print("Can not multiply")
 
