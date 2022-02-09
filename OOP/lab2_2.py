25a76b848644d1f1c35bb85f01cf8463ec9263a2
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-02-09 10:36:14 +0200

    Class MAtrix updated

--- OOP/lab2_2.py
+++ OOP/lab2_2.py
@@ -4,24 +4,24 @@ class AnyMatrix:
         self.rows = rows
         self.cols = cols
 
-    def get_matrix(self, rows, cols):
+    def __getmatrix__(self, rows, cols):
         matrix = [[0 for j in range(cols)] for i in range(rows)]
         for i in range(rows):
             for j in range(cols):
                 matrix[i][j] = int(input())
         return matrix
 
-    def check(self):
+    def __check__(self):
         negative = [[x for x in i if x < 0] for i in self.matrix]
         if len(negative) == 0:
             return True
         else:
             return False
 
-    def get_size(self):
+    def __getsize__(self):
         print(f"The given matrix has {self.rows} rows and {self.cols} columns.\n Total size: {self.rows*self.cols}")
 
-    def get_element(self, element):
+    def __getelement__(self, element):
         index = [(ind, row.index(element)) for ind, row in enumerate(self.matrix) if element in row]
         if len(index) == 0:
             print(f"Element {element} is not in the matrix")
@@ -29,31 +29,32 @@ class AnyMatrix:
             for i in range(len(index)):
                 print(f"Element {element} is at row {index[i][0]}, column {index[i][1]}")
     
-    def clear(self):
+    def __clear__(self):
         del self.matrix[:]
         self.rows, self.cols = 0, 0
         print("Matrix has been cleared")
 
-    def print(self):
+    def __print__(self):
         for i in self.matrix:
             print(*i)
 
-    def get_item(self, item1, item2):
+    def __getitem__(self, item1, item2):
         return self.matrix[item1][item2]
 
     def __del__(self):
         print("Class object (matrix) deleted")
 
+
 class Matrix(AnyMatrix):
     def __init__(self, matrix):
-        if matrix.check() == False:
+        if matrix.__check__() == False:
             print("Negative integers detected. Plese refiil the matrix")
             self.matrix = self.refill_matrix(matrix.rows, matrix.cols)
         else:
             self.matrix = matrix
             print("No negative integers detected")
 
-    def refill_matrix(self, rows, cols):
+    def __refill__(self, rows, cols):
         matrix = [[0 for j in range(cols)] for i in range(rows)]
         for i in range(rows):
             for j in range(cols):
@@ -64,5 +65,24 @@ class Matrix(AnyMatrix):
                 matrix[i][j] = x
         return matrix
             
+if __name__ == '__main__':
+    n = int(input("Enter amount of rows: "))
+    m = int(input("Enter amount of columns: "))
+    matrix = AnyMatrix(n, m)
+    print("Creating class object...\n")
+    print("Enter values: \n")
+    matrix.__getsize__()
+    print("Your matrix: \n")
+    matrix.__print__()
+    val = int(input("Enter value to get"))
+    matrix.__getelement__(val)
+    print("Checking for negative values")
+    pmatrix = Matrix(matrix)
+    print("Your matrix: \n")
+    pmatrix.__print__()
+    print("Clearing the matrix")
+    pmatrix.__clear__()
+    pmatrix.__print__()
+
 
 
