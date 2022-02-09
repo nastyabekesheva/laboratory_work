fd91c8ea7eb31ec2f83cb90d969b58e6273d753a
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-02-09 10:56:34 +0200

    Class Overload updated

--- OOP/lab3_2.py
+++ OOP/lab3_2.py
@@ -11,7 +11,7 @@ class Overload(AnyMatrix):
         if self.rows == other.rows and self.cols == other.cols:
             for i in range(self.rows):
                 for j in range(self.cols):
-                    if self.matrix.get_item(i, j) != other.matrix.get_item(i, j):
+                    if self.matrix.__getitem__(i, j) != other.matrix.__getitem__(i, j):
                         val = False
             if val == True:
                 print("Equal")
@@ -19,11 +19,10 @@ class Overload(AnyMatrix):
                 print("Not equal")
         else:
             print("Can not compare")
-        
 
     def __add__(self, other):
         if self.rows == other.rows and self.cols == other.cols:
-            add = [[self.matrix.get_item(i, j) + other.matrix.get_item(i, j) for i in range(self.cols)] for j in range(self.rows)]
+            add = [[self.matrix.__getitem__(i, j) + other.matrix.__getitem__(i, j) for i in range(self.cols)] for j in range(self.rows)]
             for i in add:
                 print(*i)
             return add
@@ -32,7 +31,7 @@ class Overload(AnyMatrix):
 
     def __sub__(self, other):
         if self.rows == other.rows and self.cols == other.cols:
-            sub = [[self.matrix.get_item(i, j) - other.matrix.get_item(i, j) for i in range(self.cols)] for j in range(self.rows)]
+            sub = [[self.matrix.__getitem__(i, j) - other.matrix.__getitem__(i, j) for i in range(self.cols)] for j in range(self.rows)]
             for i in sub:
                 print(*i)
             return sub
@@ -45,12 +44,29 @@ class Overload(AnyMatrix):
             for i in range(self.rows):
                 for j in range(other.cols):
                     for k in range(other.rows):
-                        mul[i][j] += self.matrix.get_item(i, k)*other.matrix.get_item(k, j)
+                        mul[i][j] += self.matrix.__getitem__(i, k)*other.matrix.__getitem__(k, j)
             for i in mul:
                 print(*i)
             return mul
         else:
             print("Can not multiply")
 
-
+if __name__ == '__main__':
+    n = int(input("Enter amount of rows: "))
+    m = int(input("Enter amount of columns: "))
+    u = AnyMatrix(n, m)
+    print("\nCreating class object...\n")
+    n1 = int(input("Enter amount of rows: "))
+    m1 = int(input("Enter amount of columns: "))
+    w = AnyMatrix(n1, m1)
+    print("\nCreating class object...\n")
+    uo = Overload(u)
+    wo = Overload(w)
+    print("\nOverloading addition: ")
+    uo.__add__(wo).__print__()
+    print("\nOverloading substraction: ")
+    uo.__sub__(wo).__print__()
+    print("\nOverloading multiplication: ")
+    uo.__mul__(wo).__print__()
+    print("\nOverloading comparison: ")
 
